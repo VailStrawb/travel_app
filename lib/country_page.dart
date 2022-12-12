090bc47d28900d01b98932834630fbd959c8337a
Vail Strawb <vailstrawb8@gmail.com>
2022-12-13 00:23:57 +0400

    Add a country description in a Country page

--- lib/country_page.dart
+++ lib/country_page.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/models/country.dart';
 
-class CountryPage extends StatelessWidget {
+class CountryPage extends StatefulWidget {
   const CountryPage({super.key, required this.country});
   final Country country;
+
+  @override
+  State<CountryPage> createState() => _CountryPageState();
+}
+
+class _CountryPageState extends State<CountryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                color: const Color.fromARGB(255, 44, 44, 44),
+                child: Text(
+                  'Region: ${widget.country.region}',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                color: const Color.fromARGB(255, 44, 44, 44),
+                child: Text(
+                  'Population: ${widget.country.population} people',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                color: const Color.fromARGB(255, 44, 44, 44),
+                child: Text(
+                  'Description:${widget.country.description}',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       backgroundColor: const Color.fromARGB(255, 32, 32, 32),
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white70),
         title: Text(
-          country.name,
+          widget.country.name,
           style: const TextStyle(color: Colors.white70, fontWeight: FontWeight.w600),
         ),
-        backgroundColor: const Color.fromARGB(255, 52, 52, 52),
+        backgroundColor: const Color.fromARGB(255, 44, 44, 44),
       ),
     );
   }
--- lib/models/country.dart
+++ lib/models/country.dart
@@ -3,14 +3,17 @@ class Country {
     required this.name,
     required this.region,
     required this.population,
+    required this.description,
   });
 
   final String name;
   final String region;
   final String population;
+  final String description;
 
   Country.fromList(List<dynamic> list)
       : name = list[0],
         region = list[1],
-        population = list[2].toString();
+        population = list[2].toString(),
+        description = 'Country description';
 }
